2565a7dee0b4ca66594e2f93cfc5eb881b9a7cd8
RamaArbnor <arama@york.citycollege.eu>
2022-10-29 15:49:12 +0300

    .

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -5,7 +5,15 @@ export default function Item(props){
     return(
         <div className='item-holder '>
             <div className='item'>
-                <img src={props.img} className='item-img border'/>
+                <img src={props.img} className='item-img '/>
+                <h3>Coffe</h3>
+                
+                <div className='price-bar '>
+
+                <p>Price :</p>
+                <div className = 'plus'> + </div>
+
+                </div>
             </div>
         </div>
     )
